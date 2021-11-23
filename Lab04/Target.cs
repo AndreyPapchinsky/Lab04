e0707ba737d6b099c7d94133d90a0fb019c6480a
AndreyPapchinsky <Ha.xJle6ywek@yandex.ru>
2021-11-24 02:22:38 +0300

    Добавьте файлы проекта.

---
+++ Lab04/Target.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lab04
+{
+    class target
+    {
+
+        public struct ShotRes
+        {
+            public int x;
+            public int y;
+
+        }
+        static void Main(string[] args)
+        {
+            ShotRes Shot;
+            int Score = new();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Введите значение x");
+                Shot.x = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите значение y");
+                Shot.y = int.Parse(Console.ReadLine());
+
+                double Result = Math.Sqrt(Shot.x * Shot.x + Shot.y * Shot.y);
+
+                switch (Result)
+                {
+                    case <= 5:
+                        Score += 10;
+                        break;
+
+                    case <= 10:
+                        Score += 5;
+                        break;
+
+                    case <= 30:
+                        Score += 1;
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.WriteLine("Score: " + Score);
+            }
+
+        }
+    }
+}
